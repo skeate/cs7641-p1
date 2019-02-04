5adf0b458b0b730001eda77659aa2421e8824a0c
Jonathan Skeate <skeate@gmail.com>
2019-02-03 22:16:21 -0500

    boosting

---
+++ bdt.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import ensemble
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from helpers import dtclf_pruned, basicResults, scorer
+from parse_data import read_gestures, read_wine
+from plot import plot_learning_curve, plot_timing_curve, plot_iteration_curve
+
+alphas = [x * (10 ** y) for x in [-1, 1] for y in range(-4, 4)]
+
+def run_boost(data, dataset, dtparams={}):
+    x, y, pipeline = data
+    pipe = Pipeline([
+        *pipeline,
+        ('Boost', ensemble.AdaBoostClassifier(algorithm='SAMME',base_estimator=dtclf_pruned(**dtparams))),
+    ])
+    x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y)
+    params = {
+        'Boost__n_estimators': [2 ** i for i in range(8)],
+        'Boost__algorithm': ['SAMME', 'SAMME.R'],
+    }
+    clf = basicResults(pipe, x_train, y_train, x_test, y_test, params, 'boosted', dataset)
+    # plot_learning_curve(clf, dataset + ' boosted', x, y,
+    #                     ylim=(0.0, 1.01), cv=5, n_jobs=4, scoring=scorer)
+    # plt.savefig('./graphs/' + dataset + '-boost.png')
+    plot_timing_curve(clf, x, y, 'boost', dataset)
+    plt.savefig('./graphs/' + dataset + '-boost-timing.png')
+    plot_iteration_curve(clf, x_train, y_train, x_test, y_test, params, 'boosted', dataset)
+    plt.savefig('./graphs/' + dataset + '-boost-iteration.png')
+
+if __name__ == '__main__':
+    run_boost(read_wine(), 'wine', dtparams={
+        'alpha': -100,
+        'class_weight': 'balanced',
+        'criterion': 'entropy',
+        'min_samples_split': 2
+    })
+    run_boost(read_gestures(), 'gestures', dtparams={
+        'alpha': 0.0001,
+        'class_weight': 'balanced',
+        'criterion': 'gini',
+        'min_samples_split': 4
+    })
